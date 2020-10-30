d66e8a7efef32e1969176db1eb2040007874cd39
Lucas Nunes <lucasnunes000@gmail.com>
2020-10-30 12:43:05 -0300

    Configuração inicial e criação de Modelo/Enumerações

---
+++ src/main/java/poc/rabbitmq/model/Payment.java
@@ -0,0 +1,23 @@
+package poc.rabbitmq.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import poc.rabbitmq.enumeration.PaymentOption;
+import poc.rabbitmq.enumeration.PaymentStatus;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private String id;
+    private String description;
+    private PaymentOption option;
+    private BigDecimal price;
+    private Integer quantity;
+    private PaymentStatus status;
+
+}
